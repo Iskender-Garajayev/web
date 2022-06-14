d60fee99b4e67bb9eca50c81eb60a00a7d9ae45e
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-06-15 00:26:59 +0300

    move

--- food-delivery/src/App.js
+++ food-delivery/src/App.js
@@ -3,8 +3,9 @@ import { Header } from './components';
 
 function App() {
   return (
-    <div className="w-screnn h-auto flex flex-col ">
+    <div className="w-screnn h-auto flex flex-col bg-primary ">
      <Header/>
+     <main className=' mt-24 p-8 w-full'>Maincontainer</main>
     </div>
   );
 }
