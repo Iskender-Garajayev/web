a835be24a4e05503a226f486c136c4cbb11bce64
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-06-20 08:13:00 +0300

    add delishes

---
+++ food-delivery/src/Utils/Data.js
@@ -0,0 +1,11 @@
+import I1 from '../img/i1.png'
+import F1 from '../img/f1.png'
+import C3 from '../img/c3.png'
+import FI1 from '../img/fi1.png'
+
+export const heroData =[
+  {di: 1, name: 'Iscream', desp: 'Chocolate & Vanilla', price: '5.25', imageSrc: I1 },
+  {di: 2, name: 'Strawberies', desp: 'Fresh Strawberies', price: '10.5', imageSrc: F1 },
+  {di: 3, name: 'Chicken Kebab', desp: 'Mixed Kebab Plate', price: '8.37', imageSrc: C3 },
+  {di: 4, name: 'Fesh Food', desp: 'Mixed Fish Delishes', price: '5.25', imageSrc: FI1 },
+]
