44519b3068de7e508b102c3418583fa3e180450a
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-06-29 01:22:36 +0300

    add desings

--- axios/src/components/Main.js
+++ axios/src/components/Main.js
@@ -10,8 +10,8 @@ function Main() {
     <div className='main_card_img'>
       <div className=' w-full grid grid-cols-3 h-full items-center justify-center pt-32  flex-wrap '>
         {pizzaData && pizzaData.map(n=>(
-          <div key={n.id} className="drop-shadow-lg md lg:w-100 min-w-[150px]  p-4 bg-cardOverlay backdrop-blur-md rounded-3xl flex flex-col items-center justify-center">
-            <img className='w-20 lg:w-40 -mt-10 lg:-mt-20' src={n.imgSrc} alt="p1" />
+          <div key={n.id} className="  drop-shadow-lg mt-14  bg-cardOverlay backdrop-blur-md rounded-3xl flex flex-col items-center justify-center">
+            <img className='w-20 lg:w-40 -mt-10 lg:-mt-20 ' src={n.imgSrc} alt="p1" />
             <p>{n.name}</p>
             <p>{n.desp}</p>
             <p>{n.price}</p>
