dc4cb2fbc4ba7eaffa7e672e911a642426eedbc8
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-06-29 01:28:14 +0300

    update

--- axios/src/components/Header.js
+++ axios/src/components/Header.js
@@ -1,21 +1,22 @@
-import React from 'react'
-import './Header.css'
-import Logo from '../img/logo.png'
+import React from "react";
+import "./Header.css";
+import Logo from "../img/logo.png";
 
 function Header() {
   return (
-    <div className='header'>
-
-
-      <div className='header_logo'>
-        <div className='logo'>
-          <img className='navbar_logo' src={Logo} alt="logo" />
+    <div className="header">
+      <div className="header_logo">
+        <div className="logo">
+          <img className="navbar_logo" src={Logo} alt="logo" />
         </div>
 
-
-        <div className='navbar'>
+        <div className="navbar">
           <ul>
-            <input className='search_bar' type="text" placeholder='Search Your Pizza'/>
+            <input
+              className="search_bar"
+              type="text"
+              placeholder="Search Your Pizza"
+            />
             <li>Home</li>
             <li>Menu</li>
             <li>Contacts</li>
@@ -23,15 +24,8 @@ function Header() {
           </ul>
         </div>
       </div>
-
-
-
-
-
-
-
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- axios/src/components/Main.js
+++ axios/src/components/Main.js
@@ -1,25 +1,32 @@
-import React from 'react'
+import React from "react";
 // import './Main.css'
 // import pp1 from '../img/pp1.png'
 // import p1 from '../img/p1.png'
-import {pizzaData} from '../util/PizzaData'
-
+import { pizzaData } from "../util/PizzaData";
 
 function Main() {
   return (
-    <div className='main_card_img'>
-      <div className=' w-full grid grid-cols-3 h-full items-center justify-center pt-32  flex-wrap '>
-        {pizzaData && pizzaData.map(n=>(
-          <div key={n.id} className="  drop-shadow-lg mt-14  bg-cardOverlay backdrop-blur-md rounded-3xl flex flex-col items-center justify-center">
-            <img className='w-20 lg:w-40 -mt-10 lg:-mt-20 ' src={n.imgSrc} alt="p1" />
-            <p>{n.name}</p>
-            <p>{n.desp}</p>
-            <p>{n.price}</p>
-          </div>
-        ))}
+    <div className="main_card_img">
+      <div className=" w-full grid grid-cols-3 h-full items-center justify-center pt-32  flex-wrap ">
+        {pizzaData &&
+          pizzaData.map((n) => (
+            <div
+              key={n.id}
+              className="  drop-shadow-lg mt-14  bg-cardOverlay backdrop-blur-md rounded-3xl flex flex-col items-center justify-center"
+            >
+              <img
+                className="w-20 lg:w-40 -mt-10 lg:-mt-20 "
+                src={n.imgSrc}
+                alt="p1"
+              />
+              <p>{n.name}</p>
+              <p>{n.desp}</p>
+              <p>{n.price}</p>
+            </div>
+          ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default Main
+export default Main;
