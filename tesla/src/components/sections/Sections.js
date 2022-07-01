f587b62d8b53f0a721f520d713e6caadb5146e0b
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-07-01 04:57:53 +0300

    background model s

--- tesla/src/components/sections/Sections.js
+++ tesla/src/components/sections/Sections.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import './Sections.css'
+// import tr2 from '../../img/tr2.png'
 
 function Sections() {
   return (
-    <div>Sections</div>
+    <div className='model_s'>
+      {/* model s */}
+      <div>
+        <h2>Model S</h2>
+        <p>Order Oline for <a href="http://" target="_blank" rel="noopener noreferrer">Touchless Delivery</a></p>
+        {/* <img src={tr2} alt="" /> */}
+      </div>
+    </div>
   )
 }
 
