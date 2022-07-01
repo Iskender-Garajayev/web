3587be3e922c17b2ee57fe42b362b99b7a3e1ea3
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-07-01 04:10:32 +0300

    header

--- tesla/src/components/header/Header.js
+++ tesla/src/components/header/Header.js
@@ -1,9 +1,26 @@
 import React from 'react'
 import './Header.css'
+import logo from '../../img/logo.png'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div>
+      {/* logo */}
+      <div>
+        <img className='header_logo' src={logo} alt="logo" />
+      </div>
+
+      {/* models */}
+      <div>
+
+      </div>
+
+
+      {/* navbar */}
+      <div>
+
+      </div>
+    </div>
   )
 }
 
--- tesla/src/components/main/Main.js
+++ tesla/src/components/main/Main.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Sections from '../sections/Sections'
 import './Main.css'
 
 function Main() {
   return (
-    <div>Main</div>
+    <div>
+      <Sections/>
+    </div>
   )
 }
 
