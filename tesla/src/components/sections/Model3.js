ec8480d3b84d813d0e7a245d89b915233eec0265
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-07-03 21:37:06 +0300

    style model 3

--- tesla/src/components/sections/Model3.js
+++ tesla/src/components/sections/Model3.js
@@ -3,7 +3,7 @@ import tr3 from '../../img/tr3.png'
 
 function Model3() {
   return (
-    <div>
+    <div className='model3'>
       <h2>Model 3</h2>
       <p>Order Oline for <a href="http://" target="_blank" rel="noopener noreferrer">Touchless Delivery</a></p>
       <img src={tr3} alt="Tesla" />
