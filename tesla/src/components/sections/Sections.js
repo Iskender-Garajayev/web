8e8b236f4d74f6678e54258b3a25a0b10f68c2f8
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-07-05 20:40:36 +0300

    update

--- tesla/src/components/sections/Sections.js
+++ tesla/src/components/sections/Sections.js
@@ -1,22 +1,27 @@
-import React from 'react'
-import './Sections.css'
-import tr2 from '../../img/tr2.png'
+import React from "react";
+import "./Sections.css";
+import tr2 from "../../img/tr2.png";
 
 function Sections() {
   return (
-    <div className='model_s'>
+    <div className="model_s">
       {/* model s */}
       <div>
         <h2>Model S</h2>
-        <p>Order Oline for <a href="http://" target="_blank" rel="noopener noreferrer">Touchless Delivery</a></p>
+        <p>
+          Order Oline for{" "}
+          <a href="http://" target="_blank" rel="noopener noreferrer">
+            Touchless Delivery
+          </a>
+        </p>
         <img src={tr2} alt="Tesla" />
-        <div className='btn'>
+        <div className="btn">
           <button type="submit">custem order</button>
           <button type="submit">existing investory</button>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Sections
+export default Sections;
