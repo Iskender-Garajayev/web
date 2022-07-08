a008998ad053b4c94eedbf68b2d506939968588e
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-07-08 22:11:20 +0300

    update model 3

--- tesla/src/components/sections/Model3.js
+++ tesla/src/components/sections/Model3.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import tr3 from '../../img/tr3.png'
+import './Model3.css'
 
 function Model3() {
   return (
     <div className='model3'>
-      {/* <h2>Model 3</h2>
+      <h2>Model 3</h2>
       <p>Order Oline for <a href="http://" target="_blank" rel="noopener noreferrer">Touchless Delivery</a></p>
       <img src={tr3} alt="Tesla" />
       <div className='btn'>
         <button type="submit">custem order</button>
         <button type="submit">existing investory</button>
-      </div> */}
+      </div>
     </div>
   )
 }
