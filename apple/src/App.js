277a81aa1a5c5d3109ec9d0e8fc081ec03309822
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-07-09 18:38:03 +0300

    Header

--- apple/src/App.js
+++ apple/src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        
+        <div className=' mt-1 bg-blue-500 w-full h-4'>
+            <p className=''>name</p>
+        </div>
     </div>
   );
 }
