0b3858baa6a9f3b9a94333b2c4372d8915729068
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-07-09 19:47:00 +0300

    heaader

--- apple/src/components/header/Header.js
+++ apple/src/components/header/Header.js
@@ -2,8 +2,19 @@ import React from 'react'
 
 function Header() {
   return (
-    <div className=' w-full h-12 bg-slate-500'>
-
+    <div className=' w-full h-12 bg-black'>
+      <ul className='flex ml-auto mr-auto '>
+        <li className=' pl-6 items-center text-white'>Stote</li>
+        <li className=' pl-6 items-center text-white'>Mac</li>
+        <li className=' pl-6 items-center text-white'>iPad</li>
+        <li className=' pl-6 items-center text-white'>iPhone</li>
+        <li className=' pl-6 items-center text-white'>Whatch</li>
+        <li className=' pl-6 items-center text-white'>AirPods</li>
+        <li className=' pl-6 items-center text-white'>TV & Home</li>
+        <li className=' pl-6 items-center text-white'>Only on Apple</li>
+        <li className=' pl-6 items-center text-white'>Accesories</li>
+        <li className=' pl-6 items-center text-white'>Support</li>
+      </ul>
     </div>
   )
 }
