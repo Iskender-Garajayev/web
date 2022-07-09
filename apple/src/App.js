ddb0868bb27499171617e9a7e437fe3e9fa297d9
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-07-09 18:59:42 +0300

    update

--- apple/src/App.js
+++ apple/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
         <div className=' mt-1 bg-blue-500 w-full h-4'>
           <Header/>
-          <p className=''>name</p>
+          <p className=' bg-blue-500'>name</p>
         </div>
     </div>
   );
