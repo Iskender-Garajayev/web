bf170ed7e1e721b23b1ec12bda3688c2b2a557a4
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-07-10 10:03:21 +0300

    header icons

--- apple/src/App.js
+++ apple/src/App.js
@@ -4,7 +4,7 @@ import Header from './components/header/Header'
 function App() {
   return (
     <div className="App">
-        <div className=' '>
+        <div className=''>
           <Header/>
         </div>
     </div>
