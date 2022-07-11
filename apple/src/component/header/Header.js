2562a7f43c8ba2520296e7edaa95ad70ea78e5f8
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-07-11 04:25:12 +0300

    reupdate

---
+++ apple/src/component/header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Router,Route} from 'react-router-dom'
+
+function header() {
+  return (
+    <Router>
+      <Route/>
+    </Router>
+  )
+}
+
+export default header
