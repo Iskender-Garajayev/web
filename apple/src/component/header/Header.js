90f4ac25ff63ceb9374a5bb804354662d283fa66
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-07-13 01:29:49 +0300

    icons

--- apple/src/component/header/Header.js
+++ apple/src/component/header/Header.js
@@ -1,29 +1,27 @@
 import React from 'react'
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import Store from '../sections/Store'
+import {AiFillApple} from 'react-icons/ai'
 
 function header() {
   return (
     <Router>
     <div className=' text-gray-300 flex items-center content-center ml-auto mr-auto w-full h-12 bg-zinc-800'>
-      
-      <ul className=' flex space-x-9 items-center content-center ml-auto mr-auto'>
-      <svg className=' hover:text-white ' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-apple" viewBox="0 0 16 16">
-      <path d="M11.182.008C11.148-.03 9.923.023 8.857 1.18c-1.066 1.156-.902 2.482-.878 2.516.024.034 1.52.087 2.475-1.258.955-1.345.762-2.391.728-2.43zm3.314 11.733c-.048-.096-2.325-1.234-2.113-3.422.212-2.189 1.675-2.789 1.698-2.854.023-.065-.597-.79-1.254-1.157a3.692 3.692 0 0 0-1.563-.434c-.108-.003-.483-.095-1.254.116-.508.139-1.653.589-1.968.607-.316.018-1.256-.522-2.267-.665-.647-.125-1.333.131-1.824.328-.49.196-1.422.754-2.074 2.237-.652 1.482-.311 3.83-.067 4.56.244.729.625 1.924 1.273 2.796.576.984 1.34 1.667 1.659 1.899.319.232 1.219.386 1.843.067.502-.308 1.408-.485 1.766-.472.357.013 1.061.154 1.782.539.571.197 1.111.115 1.652-.105.541-.221 1.324-1.059 2.238-2.758.347-.79.505-1.217.473-1.282z"/>
-      <path d="M11.182.008C11.148-.03 9.923.023 8.857 1.18c-1.066 1.156-.902 2.482-.878 2.516.024.034 1.52.087 2.475-1.258.955-1.345.762-2.391.728-2.43zm3.314 11.733c-.048-.096-2.325-1.234-2.113-3.422.212-2.189 1.675-2.789 1.698-2.854.023-.065-.597-.79-1.254-1.157a3.692 3.692 0 0 0-1.563-.434c-.108-.003-.483-.095-1.254.116-.508.139-1.653.589-1.968.607-.316.018-1.256-.522-2.267-.665-.647-.125-1.333.131-1.824.328-.49.196-1.422.754-2.074 2.237-.652 1.482-.311 3.83-.067 4.56.244.729.625 1.924 1.273 2.796.576.984 1.34 1.667 1.659 1.899.319.232 1.219.386 1.843.067.502-.308 1.408-.485 1.766-.472.357.013 1.061.154 1.782.539.571.197 1.111.115 1.652-.105.541-.221 1.324-1.059 2.238-2.758.347-.79.505-1.217.473-1.282z"/>
-      </svg>
+     
+      <ul className='  flex space-x-9 items-center content-center ml-auto mr-auto'>
+        <AiFillApple className=' hover:text-white' size="20"  />
         <li className=' hover:text-white'>
           <Link to='/Store'>{Store}Store</Link>
           </li>
-        <li className=' hover:text-white'>Mac</li>
-        <li className=' hover:text-white'>iPad</li>
-        <li className=' hover:text-white'>iPhone</li>
-        <li className=' hover:text-white'>Whatch</li>
-        <li className=' hover:text-white'>AirPods</li>
-        <li className=' hover:text-white'>TV & Home</li>
-        <li className=' hover:text-white'>Only on Apple</li>
-        <li className=' hover:text-white'>Accesories</li>
-        <li className=' hover:text-white'>Support</li>
+        <li className=' cursor-pointer hover:text-white'>Mac</li>
+        <li className=' cursor-pointer hover:text-white'>iPad</li>
+        <li className=' cursor-pointer hover:text-white'>iPhone</li>
+        <li className=' cursor-pointer hover:text-white'>Whatch</li>
+        <li className=' cursor-pointer hover:text-white'>AirPods</li>
+        <li className=' cursor-pointer hover:text-white'>TV & Home</li>
+        <li className=' cursor-pointer hover:text-white'>Only on Apple</li>
+        <li className=' cursor-pointer hover:text-white'>Accesories</li>
+        <li className=' cursor-pointer hover:text-white'>Support</li>
 
         <svg className=' hover:text-white' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
       <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
