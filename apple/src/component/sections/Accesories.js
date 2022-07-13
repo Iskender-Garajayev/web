a950a364fae94edb5ba144aa4667140c42d50de2
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-07-13 06:13:36 +0300

    update

--- apple/src/component/sections/Accesories.js
+++ apple/src/component/sections/Accesories.js
@@ -6,7 +6,7 @@ function Accesories() {
     <div className=' flex justify-between ml-auto mr-auto  content-center items-center w-4/5 '>
       <h3 className=' text-2xl cursor-pointer'>Accesories</h3>
       <h4 className='flex cursor-pointer content-center items-center text-sm hover:text-gray-500'>Browse all <AiOutlineDown className=' pl-1 pt-1' size='10' /> </h4>
-      
+      <h4>name </h4>
     </div>
   )
 }
