f6af273631b64174eba8dfad2c635b6c1e1ed8b7
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-07-14 04:17:56 +0300

    Store Update

--- apple/src/component/sections/Store.js
+++ apple/src/component/sections/Store.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Store() {
   return (
-    <div>Store</div>
+    <div className=' grid grid-cols-6 gap-4 w-4/5 ml-auto mr-auto pt-32'>
+      <div className=' col-start-1 col-end-5 text-5xl font-bold text-gray-600 '><span className=' text-5xl font-bold text-black' >Store. </span>The best wey to buy the products you love.</div>
+      <div className=' col-end-7 col-span-2' ></div>
+    </div>
   )
 }
 
