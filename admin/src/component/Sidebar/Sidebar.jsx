34430e6120e007ef1b24045f57d238d3cb722523
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-07-28 00:38:02 +0300

    update

--- admin/src/component/Sidebar/Sidebar.jsx
+++ admin/src/component/Sidebar/Sidebar.jsx
@@ -5,9 +5,20 @@ import Logo from '../../img/logo.png'
 const Sidebar = () => {
   return (
     <div className='Sidebar'>
+      {/* Logo */}
       <div className='logo' >
         <img src={Logo} alt="logo" />
-        <span>Sh<span>o</span>ps</span>
+        <span>Z<span>oo</span> Sh<span>o</span>ps</span>
+      </div>
+      {/* menu */}
+
+      <div className="menu">
+        <div className="menuItem">
+          <div>
+            icons
+          </div>
+          <span>Dashbord</span>
+        </div>
       </div>
     </div>
   )
