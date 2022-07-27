ce25df7356e970c41e4056fb52f44953e840b31e
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-07-27 04:22:25 +0300

    update

--- admin/src/App.js
+++ admin/src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className='AppGlass' ></div>
+      <div className='AppGlass' >
+        
+      </div>
     </div>
   );
 }
